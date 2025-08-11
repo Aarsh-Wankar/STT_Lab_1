85901e2d20eef2b19a03c6dccdda4acffedf7c41
Aarsh-Wankar <wankar.aarsh@gmail.com>
2025-08-11 15:22:56 +0530

    fixed pylint errors

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 def fibonacci(n):
-    return fibonacci(n-1) + fibonacci(n-2) #infinite recursion
+    if n == 0: return 0
+    if n == 1: return 1 
 
-def gibberish(n):
-    vector<int> h(n);           #C++!!!!????
-    for(int i = 0; i<n; i++){
-        h[i] = i;
-    }
-    return h;
+    return fibonacci(n-1) + fibonacci(n-2) #fixed infinite recursion
+
+# def gibberish(n):
+#     vector<int> h(n);           #C++!!!!????
+#     for(int i = 0; i<n; i++){
+#         h[i] = i;
+#     }
+#     return h;
 
 def main():
     n = int(input("Enter a number: "))
     return fibonacci(n)
 
-
 if __name__ == "main":
     main()
-    gibberish(10)
-
-
-
-i = 5           #Unused variable
-
-
+    # gibberish(10)
